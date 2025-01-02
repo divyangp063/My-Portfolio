b9e8a537b387b8451077cc9dd72c1938ba402a2a
divyangp063 <prajapati.30@buckeyemail.osu.edu>
2025-01-02 13:06:39 -0500

    Implemented Animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,6 +145,8 @@ const sr = ScrollReveal({
     // reset: true // Animations repeat
 })
 
-sr.reveal(`.home__perfil, .about__image`, {origin: 'right'})
+sr.reveal(`.home__perfil, .about__image, .contact__mail`, {origin: 'right'})
 sr.reveal(`.home__name, .home__info, 
-            .about__container .section__title-1, .about__info`, {origin: 'left'})
+            .about__container .section__title-1, .about__info,
+            .contact__social, .contact__data`, {origin: 'left'})
+sr.reveal(`.services__card, .projects__card`, {interval: 100})
